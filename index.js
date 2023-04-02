73000a71a7d9fe24397954d9e7a96d74a6711fdf
Md Hadiuzzaman Rifat <hadirifat100@gmail.com>
2023-04-02 16:12:07 +0600

    make a few changes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const { application } = require("express");
+const ObjectId  = require("mongodb").ObjectId;
+
 app.use(express.json());
 app.use(cors());
 
@@ -15,11 +16,12 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+const database = client.db("hadi");
+const person = database.collection("israt");
+
 app.post("/appointments", (req, res) => {
   async function run() {
     try {
-      const database = client.db("hadi");
-      const person = database.collection("israt");
       const body = req.body;
       const result = person.insertOne(body);
       res.send(result);
@@ -33,17 +35,68 @@ app.post("/appointments", (req, res) => {
 app.get("/appointments", (req, res) => {
   async function run() {
     try {
-      const database = client.db("hadi");
-      const person = database.collection("israt");
-      const query=req.query
+      const query = req.query;
       const cursor = person.find(query);
       const appointments = await cursor.toArray();
-      res.send(appointments)
+      res.send(appointments);
     } catch {
       console.log("failed to write");
     }
   }
   run();
+});
+
+// payment of booking search by id
+app.get("/appointment/bookingPayment/:id", (req, res) => {
+  async function run() {
+    const _id=new ObjectId(req.params.id)
+  
+    const result= await person.findOne({_id})
+    res.send(result)
+  }
+  run();
+});
+
+app.get("/appointments", async (req, res) => {
+  const query = req.query.name;
+  const result = await person.findOne(query);
+  res.json(result);
+});
+
+app.get("/users", async (req, res) => {
+  async function run() {
+    res.send("hello world");
+  }
+  run();
+});
+
+app.post("/users", async (req, res) => {
+  async function run() {
+    try {
+      const user = req.body;
+      const result = await person.insertOne(user);
+      console.log(result);
+      res.json(result);
+    } catch {
+      console.log("failed to write user");
+    }
+  }
+  run();
+});
+
+app.put("/users", async (req, res) => {
+  async function run() {
+    try {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await person.updateOne(filter, updateDoc, options);
+      res.json(result);
+    } catch {
+      console.log("failed to update user");
+    }
+  }
 });
 
 const PORT = process.env.port || 2020;
